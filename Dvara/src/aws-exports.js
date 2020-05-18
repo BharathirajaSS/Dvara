c620d90f162b986782124e60d4af61d7775c7d4b
Bharathiraja SS <54929507+BharathirajaSS@users.noreply.github.com>
2020-05-18 09:25:23 +0530

    expo aws congigure

---
+++ Dvara/src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsConfig = {
+    identityPoolId: 'ap-south-1:23da2a80-1713-4552-b2c1-ce3c045dd61c',
+    region: 'ap-south-1',
+    userPoolId: 'ap-south-1_wCFz3lNOg',
+    userPoolWebClientId: '1g622klerofn1dp5gqbvi0maa0mpebl3i3df27b77d79opbuen94'
+  }
+  
+  export default awsConfig;
